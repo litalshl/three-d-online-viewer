37ee2e547fc5da6a573190855dd027d80964e552
Lital <lital@cherryimaging.com>
2020-11-12 14:41:17 +0200

    Remove unneeded files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { LastPatientModelComponent } from './last-patient-model/last-patient-model.component';
 import { ModelViewComponent } from './components/model-view/model-view.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LastPatientModelComponent,
     ModelViewComponent
   ],
   imports: [
